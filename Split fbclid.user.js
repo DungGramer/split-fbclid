7bb1e29b87d47ed3e65b488de49e93e491163b7b
Nguyễn Công Dũng <dung.dev.gramer@gmail.com>
2020-11-02 10:14:43 +0700

    Add files via upload

---
+++ Split fbclid.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Split fbclid
+// @author       DungGramer
+// @version      1.0
+// @description  Split fbclid when open link from facebook
+// @homepage     https://greasyfork.org/en/scripts/415274-split-fbclid
+// @namespace    https://www.facebook.com/dung.dev.gramer/
+// @icon         https://www.flaticon.com/svg/static/icons/svg/889/889578.svg
+// @include      /((&fbclid=\w+)|(fbclid=\w+))(-\w+|)/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    let newURL = window.location.href.split(/((&fbclid=\w+)|(fbclid=\w+))(-\w+|)/);
+    let newHref = newURL[0].match(/\?\w+/) != null ? decodeURIComponent(newURL[0]) : newURL[0].split(/\?/)[0];
+    if(newURL[newURL.length - 1].match(/-\w+/) != null) {
+      window.location = decodeURIComponent(newHref);
+    } else {
+        window.location = decodeURIComponent(newHref + newURL[newURL.length - 1]);
+    }
+})();
