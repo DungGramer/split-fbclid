d29c6d2b84d99b9855d3be7408dd6bbe17dbb8cb
Nguyễn Công Dũng <dung.dev.gramer@gmail.com>
2020-11-02 11:46:27 +0700

    v 1.2

--- Split fbclid.user.js
+++ Split fbclid.user.js
@@ -1,24 +1,21 @@
 // ==UserScript==
 // @name         Split fbclid
 // @author       DungGramer
-// @version      1.1
+// @version      1.2
 // @description  Split fbclid when open link from facebook
-// @homepage     https://github.com/DungGramer/split-fbclid
-// @icon         https://raw.githubusercontent.com/DungGramer/skip-fbclid/main/icon.png
+// @homepage     https://greasyfork.org/en/scripts/415274-split-fbclid
+// @namespace    https://www.facebook.com/dung.dev.gramer/
+// @icon         https://www.flaticon.com/svg/static/icons/svg/889/889578.svg
 // @include      /((&fbclid=\w+)|(fbclid=\w+))(-\w+|)/
-// @license      CC-BY-SA-3.0; http://creativecommons.org/licenses/by-sa/3.0/
-// @license      MIT
 // @grant        none
-// @copyright    2020, DungGramer
 // ==/UserScript==
 
-(function () {
-  let newURL = window.location.href.split(/((&fbclid=\w+)|(fbclid=\w+))(-\w+|)/);
-  let newHref = newURL[0].match(/\?\w+/) != null ? decodeURIComponent(newURL[0]) : newURL[0].split(/\?/)[0];
-  if (newURL[newURL.length - 1].match(/-\w+/) != null) {
-    window.location = decodeURIComponent(newHref);
-  }
-  else {
-    window.location = decodeURIComponent(newHref + newURL[newURL.length - 1]);
-  }
+(function() {
+    let newURL = window.location.href.split(/((&fbclid=\w+)|(fbclid=\w+))(-\w+|)/);
+    let newHref = newURL[0].match(/\?\w+/) != null ? newURL[0] : newURL[0].split(/\?/)[0];
+    if(newURL[newURL.length - 1].match(/-\w+/) != null) {
+        history.replaceState(null, '', newHref);
+    } else {
+         history.replaceState(null, '', newHref + newURL[newURL.length - 1]);
+    }
 })();
